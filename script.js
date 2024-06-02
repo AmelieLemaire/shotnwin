113d97b05da00ecacaf862915701ff456fcd8232
Amélie <lemaireamelie85@gmail.com>
2024-06-02 17:19:36 +0200

    ajout cookie pour score

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     let btn = document.querySelector('.start_btn');
     let scoreContainer = document.querySelector('.score');
     let timeContainer = document.querySelector('.time');
+    let bestScoreContainer = document.querySelector('.best_score');
+
+    // Mettre à jour le meilleur score à l'ouverture de la page
+    function updateBestScoreDisplay() {
+        let bestScore = getCookie('bestScore');
+        bestScoreContainer.innerHTML = 'Meilleur Score : ' + (bestScore ? bestScore : 0);
+    }
+
+    updateBestScoreDisplay();
 
     btn.onclick = function () {
         let backgroundAudio = new Audio('https://universal-soundbank.com/sounds/5198.mp3');
@@ -34,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
             target.onclick = function () {
                 score += 1;
                 target.style.display = 'none';
-                scoreContainer.innerHTML = 'Score: ' + score;
+                scoreContainer.innerHTML = 'Score : ' + score;
                 
                 let audio = new Audio('https://universal-soundbank.com/sounds/3570.mp3');
                 audio.play();
@@ -50,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         let gameInterval = setInterval(function() {
             time -= 1;
-            timeContainer.innerHTML = 'Time: ' + time;
+            timeContainer.innerHTML = 'Temps : ' + time;
 
             if (time === 0) {
                 clearInterval(gameInterval);
@@ -69,9 +78,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 let audio = new Audio('https://universal-soundbank.com/sounds/253.mp3');
                 audio.play();
+
+                let bestScore = getCookie('bestScore');
+                if (!bestScore || score > bestScore) {
+                    setCookie('bestScore', score, 365);
+                    updateBestScoreDisplay();
+                }
             }
         }, 1000);
 
         let targetInterval = setInterval(showTarget, intervalTime);
     };
+
+    // Fonction pour définir un cookie
+    function setCookie(name, value, days) {
+        let expires = "";
+        if (days) {
+            let date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+
+    // Fonction pour obtenir un cookie
+    function getCookie(name) {
+        let nameEQ = name + "=";
+        let ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
 });
